9d9da6585617997d82622d47fd8dc84b1dac5896
cmh3258 <chume@utexas.edu>
2014-02-03 13:11:16 -0600

    adding blankslate theme

---
+++ wp-content/themes/blankslate/footer.php
@@ -0,0 +1,20 @@
+
+    <div class="clear"></div>
+    </div>
+
+    <footer id="footer" role="contentinfo">
+        <!--
+        <div id="copyright">
+            <?php echo sprintf( __( '%1$s %2$s %3$s. All Rights Reserved.', 'blankslate' ), '&copy;', date( 'Y' ), esc_html( get_bloginfo( 'name' ) ) ); echo sprintf( __( ' Theme By: %1$s.', 'blankslate' ), '<a href="http://tidythemes.com/">TidyThemes</a>' ); ?>
+        </div>
+        -->
+        <div id="footer_content">
+            <h4>Contact us:<span class="tab"></span><a href="https://twitter.com/RecommenuApp/">Twitter</a><span class="tab"></span><a href="https://www.facebook.com/recommenuapp">Facebook</a><span class="tab"></span><a href="mailto:mail.recommenu@gmail.com?Subject=Recommenu%20Notification">Email</a></h4>
+        </div>
+        
+    </footer>
+</div>
+
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ wp-content/themes/blankslate/index.php
@@ -0,0 +1,22 @@
+<div class="header_style">
+    <?php get_header(); ?>
+</div>    
+
+<section id="content" role="main">
+    
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+    <!--
+    <?php get_template_part('entry'); ?>
+    <?php comments_template(); ?>
+    -->
+    <div id="content_section">
+    <h1><?php the_title(); ?></h1>
+    <h4>Posted on <?php the_time('F jS, Y') ?></h4>
+<p><?php the_content(__('(more...)')); ?></p>
+    </div>
+<?php endwhile; endif; ?>
+    
+<?php get_template_part('nav', 'below'); ?>
+</section>
+<!--<?php get_sidebar(); ?>-->
+<?php get_footer(); ?>
